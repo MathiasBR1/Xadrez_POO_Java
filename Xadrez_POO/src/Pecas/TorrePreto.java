d23c2d7e03bb47b6e8afaae3115434086a116195
Arthur Correia <156236396+arthurscorreia@users.noreply.github.com>
2025-07-07 11:29:16 -0300

    movimentacao torre preta

--- Xadrez_POO/src/Pecas/TorrePreto.java
+++ Xadrez_POO/src/Pecas/TorrePreto.java
@@ -1,13 +1,84 @@
 package Pecas;
 
+import Tabuleiro.Casa;
 import Tabuleiro.ChessBoard;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class TorrePreto extends Peca {
     public TorrePreto(int row, int col, ImageIcon imagem) {
         super(row, col, "Torre", "Preto", imagem);
     }
 
-    public void move(ChessBoard tabuleiro) {}
+    public void move(ChessBoard tabuleiro) {
+        tabuleiro.startmoving();
+        ArrayList<Casa> movimentos = new ArrayList<>();
+        // implementação dos movimentos da peça Torre Branca - enquanto não houver peça da mesma cor nos seus possíveis movimentos, 
+        // se movimenta para casas nas direções horizontais e verticais. quando encontra uma peça de cor contrária, para a movimentação para captura.
+        int i = row - 1;
+        while (i >= 0) {
+            if (tabuleiro.getPeca(i, col) == null) {
+                Casa aux = new Casa(i, col);
+                movimentos.add(aux);
+            }
+            else {
+                if (tabuleiro.getPeca(i, col).getColor().equals("Branco")) {
+                    Casa aux = new Casa(i, col);
+                    movimentos.add(aux);
+                }
+                break;
+            }
+            i -= 1;
+        }
+
+        i = row + 1;
+        while (i <= 7) {
+            if (tabuleiro.getPeca(i, col) == null) {
+                Casa aux = new Casa(i, col);
+                movimentos.add(aux);
+            }
+            else {
+                if (tabuleiro.getPeca(i, col).getColor().equals("Branco")) {
+                    Casa aux = new Casa(i, col);
+                    movimentos.add(aux);
+                }
+                break;
+            }
+            i += 1;
+        }
+
+        i = col - 1;
+        while (i >= 0) {
+            if (tabuleiro.getPeca(row, i) == null) {
+                Casa aux = new Casa(row, i);
+                movimentos.add(aux);
+            }
+            else {
+                if (tabuleiro.getPeca(row, i).getColor().equals("Branco")) {
+                    Casa aux = new Casa(row, i);
+                    movimentos.add(aux);
+                }
+                break;
+            }
+            i -= 1;
+        }
+
+        i = col + 1;
+        while (i <= 7) {
+            if (tabuleiro.getPeca(row, i) == null) {
+                Casa aux = new Casa(row, i);
+                movimentos.add(aux);
+            }
+            else {
+                if (tabuleiro.getPeca(row, i).getColor().equals("Branco")) {
+                    Casa aux = new Casa(row, i);
+                    movimentos.add(aux);
+                }
+                break;
+            }
+            i += 1;
+        }
+        visualizarmovimentos(tabuleiro, movimentos);
+    }
 }
